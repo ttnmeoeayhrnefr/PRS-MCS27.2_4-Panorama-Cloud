3f91e4455341990aab2be31cc2bd8a2f4fff20be
Théo MENANT--FERRY <t.menantferry@ecole-ipssi.net>
2025-10-16 16:05:59 +0200

    remove terraform.bak

--- terraform.bak/table_route.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route_table" "GreenShop-privrtb" {
-  vpc_id = aws_vpc.GreenShop-vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.GreenShop-natgw.id
-  }
-}
--- terraform.bak/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "allowed_ssh_ips" {
-  description = "List of IP addresses allowed to SSH to admin instance"
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
-}
