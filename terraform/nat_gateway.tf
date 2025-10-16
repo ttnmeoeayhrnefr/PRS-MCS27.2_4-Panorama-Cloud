518d365fdeb21c1c5427080080c121a128341f92
Théo MENANT--FERRY <t.menantferry@ecole-ipssi.net>
2025-10-16 12:44:13 +0200

    starting tp

--- terraform/nat_gateway.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_nat_gateway" "GreenShop-natgw" {
-  subnet_id     = aws_subnet.GreenShop-pub.id
-  allocation_id = aws_eip.GreenShop-nateip.id
-
-  tags = {
-    Name = "GreenShop-natgw"
-  }
-}
